1d35399cbea33008850c5c4b9818aadf6dc23266
KiddoKenshin <kiddokenshin@KiddoKenshins-MacBook.local>
2017-10-17 16:05:35 +0900

    Initialize ruby project.

---
+++ spec/arabic2english_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe Arabic2english do
+  it "has a version number" do
+    expect(Arabic2english::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
